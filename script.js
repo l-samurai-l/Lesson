4b5bc6948592eaf431b9c57dca5a746a5b631ad2
samurai <email@gmail.com>
2021-02-05 15:37:07 +0200

    013

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 "use strict";
 
-console.log('hi');
+if (4 == 4) {
+    console.log('OK!');    
+} else {
+    console.log('error');
+}
+
+// let num = 51;
+
+// // (num === 50) ? console.log('OK!') : console.log('Error');
+
+// switch (num) {
+//     case 49: 
+//         console.log('err');
+//         break;
+//     case 100: 
+//         console.log('err');
+//         break;
+//     case 50: 
+//         console.log('yes');
+//         break;
+//     default:
+//         console.log('no');
+//         break;
+// }
+
+// 013
